f6cb22620ff8034c48c340c7892eb5c6ce1b98ba
MrManoj124 <manojmanorooban@gmail.com>
2026-04-12 13:15:59 +0530

    Add a create a sayMyName function

--- Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
+++ Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
@@ -6,5 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BeginController {
     @GetMapping("/hello")
-
+    String sayMyName(){
+        return "Manorooban";
+    }
 }
