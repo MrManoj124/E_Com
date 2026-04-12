75c17d762187821f5e808d7ef88f3ff54074a9dd
MrManoj124 <manojmanorooban@gmail.com>
2026-04-12 13:14:31 +0530

    Add a GetMapping Annotation

--- Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
+++ Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
@@ -1,8 +1,10 @@
 package Mr.Manoj.Begin.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BeginController {
+    @GetMapping("/hello")
 
 }
