73a8a11a244b5bd3e51190799797c3a4e10650f7
MrManoj124 <manojmanorooban@gmail.com>
2026-04-13 21:42:22 +0530

    Add a end point for sayMyName method

--- Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
+++ Begin/src/main/java/Mr/Manoj/Begin/Controller/BeginController.java
@@ -1,12 +1,17 @@
 package Mr.Manoj.Begin.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping
 public class BeginController {
-    @GetMapping("/hello")
+    @GetMapping("/hello/name")
     String sayMyName(){
         return "Manorooban";
     }
+
+    @GetMapping("hello/")
 }
