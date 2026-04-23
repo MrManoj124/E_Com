7cbe3f050489a870f7978c27990561466d78f63d
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 23:17:11 +0530

    Add a addProduct method

--- demo/src/main/java/com/example/demo/service/productService.java
+++ demo/src/main/java/com/example/demo/service/productService.java
@@ -1,13 +1,19 @@
 package com.example.demo.service;
 
 
+import com.example.demo.model.product;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class productService {
-    private List<product> Product = new ArrayList<>();
+    private List<product> productList = new ArrayList<>();
 
     public String addProduct(product Product){
         return "Successfully add product";
     }
+
+
 }
