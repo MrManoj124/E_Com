40194b88ae7d254a673d81a3ad408c066780936f
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 23:02:59 +0530

    Add a List for send a data

--- demo/src/main/java/com/example/demo/service/productService.java
+++ demo/src/main/java/com/example/demo/service/productService.java
@@ -1,4 +1,13 @@
 package com.example.demo.service;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class productService {
+    private List<product> Product = new ArrayList<>();
+
+    public String addProduct(product Product){
+        return "Successfully add product";
+    }
 }
