da3891cf4fc193ce1773a6a29ae62bf653011fe3
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 22:49:40 +0530

    create variables using private method in product file

--- demo/src/main/java/com/example/demo/model/product.java
+++ demo/src/main/java/com/example/demo/model/product.java
@@ -1,4 +1,18 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import java.lang.annotation.Documented;
+
+//@Documented (collection = "products")
 public class product {
+
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public product(){}
+
+
 }
