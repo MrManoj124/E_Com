677e5b51d58008f7b108f8b8c6059b91eef316a2
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 22:53:07 +0530

    Add getter and setter for price and quantity

--- demo/src/main/java/com/example/demo/model/product.java
+++ demo/src/main/java/com/example/demo/model/product.java
@@ -24,9 +24,21 @@ public class product {
     public String getName(){
         return name;
     }
-    public void setPrice(String name){
+    public void setName(String name){
         this.name=name;
     }
 
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
 
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
 }
