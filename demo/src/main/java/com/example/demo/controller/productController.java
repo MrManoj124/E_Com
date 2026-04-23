84835e6906ebc5eebd0642dccb384c333eb7455d
MrManoj124 <manojmanorooban@gmail.com>
2026-04-23 23:22:16 +0530

    import productService file into controller

--- demo/src/main/java/com/example/demo/controller/productController.java
+++ demo/src/main/java/com/example/demo/controller/productController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.productService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
 public class productController {
+
+    private final productService ProductService;
 
 }
