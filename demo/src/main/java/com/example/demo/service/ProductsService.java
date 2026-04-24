a5ee52d3168e0bb70d480126ad8934201aacf499
MrManoj124 <manojmanorooban@gmail.com>
2026-04-24 07:42:37 +0530

    Create a list and method for addProduct

--- demo/src/main/java/com/example/demo/service/ProductsService.java
+++ demo/src/main/java/com/example/demo/service/ProductsService.java
@@ -9,7 +9,15 @@ import java.util.List;
 @Service
 public class ProductsService {
 
+    private List<Products> ProductList = new ArrayList<>();
 
+    public String addProduct(Products product){
+        if(product.getSize() <= 0){
+            return "Size must be greater than zero";
+        }
+        Products.add(product);
+        return "Size must be greater than zero";
+    }
 
 
 }
