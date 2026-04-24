cb088121aa68fd9880da80ed637b070e34d8a392
MrManoj124 <manojmanorooban@gmail.com>
2026-04-24 07:24:22 +0530

    Add getters and setters for all the variables

--- demo/src/main/java/com/example/demo/model/Products.java
+++ demo/src/main/java/com/example/demo/model/Products.java
@@ -7,6 +7,40 @@ public class Products {
     private String type;
     private String size;
 
+    public Products() {};
 
+    public Products(int id, String name, String type, String size){
+        this.id=id;
+        this.name=name;
+        this.type=type;
+        this.size=size;
+    }
 
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+
+    public String getSize(){
+        return size;
+    }
+    public void setSize(String size){
+        this.size=size;
+    }
 }
