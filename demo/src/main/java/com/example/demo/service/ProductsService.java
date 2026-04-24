fa98a80b725286bd72edb83efdac91ce9a16f9be
MrManoj124 <manojmanorooban@gmail.com>
2026-04-24 07:41:54 +0530

    Add a Service annotation

--- demo/src/main/java/com/example/demo/service/ProductsService.java
+++ demo/src/main/java/com/example/demo/service/ProductsService.java
@@ -1,4 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Products;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProductsService {
+
+
+
+
 }
