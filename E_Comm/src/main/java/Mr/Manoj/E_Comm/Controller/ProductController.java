d475381bc0aa78831eb841e144951e8a86b4af9a
MrManoj124 <manojmanorooban@gmail.com>
2026-05-13 00:00:26 +0530

    Add RequestMapping Annotation

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
@@ -1,8 +1,10 @@
 package Mr.Manoj.E_Comm.Controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 }
