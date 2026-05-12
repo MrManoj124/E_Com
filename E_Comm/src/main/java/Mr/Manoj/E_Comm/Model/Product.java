6fce59f6c5357202c8778834494137e8a2c56f7d
MrManoj124 <manojmanorooban@gmail.com>
2026-05-12 23:43:45 +0530

    create Getters and Setters in Product.java file

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Product.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Product.java
@@ -17,5 +17,38 @@ public class Product {
         this.price=price;
     }
 
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
 
 }
