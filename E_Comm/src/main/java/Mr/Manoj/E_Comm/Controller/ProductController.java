11daaaa6d58c42dc68cb054f545aa381fb1e9247
MrManoj124 <manojmanorooban@gmail.com>
2026-05-20 00:42:54 +0530

    Add Autowired Annotation

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
@@ -1,10 +1,20 @@
 package Mr.Manoj.E_Comm.Controller;
 
 
+import Mr.Manoj.E_Comm.Model.Product;
+import Mr.Manoj.E_Comm.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private ProductService productservice;
+
+    private List<Product> Products = new ArrayList<>();
 }
