4dfa40cfd1d2493cf2407a673b5fbe74cf953696
MrManoj124 <manojmanorooban@gmail.com>
2026-05-20 18:50:48 +0530

    Comment all the Annotations in ProductController.java file

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
@@ -12,38 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    @Autowired
-    private ProductService productservice;
 
-    private List<Product> Products = new ArrayList<>();
-
-    // Sending or Posing the data
-    @PostMapping('/addProduct')
-    public String PostProduct(@RequestBody Product product01){
-        if(product01.getQuantity() < 0){
-            return "Quantity cannot be negative. cannot add the product";
-        }
-        productservice.addProduct(product01);
-        return "Successfully added the datas";
-    }
-
-    // Retriew the datas
-    @GetMapping("/getProduct")
-    public List<Product>getProduct(){
-        return productservice.getProducts();
-    }
-
-    // Delete the data
-    @DeleteMapping("/deleteProduct/{id}")
-    public String deleteProduct(@PathVariable int Id){
-        boolean removed = productService.deleteProductById(int Id){
-            if(removed){
-                return("Product deleted succesfully");
-            }
-            else{
-                return"Product not found.";
-            }
-        }
-    }
 
 }
