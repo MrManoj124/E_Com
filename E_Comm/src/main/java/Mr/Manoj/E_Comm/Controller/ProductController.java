81b3cea83f4edadb7ad3b1b219f7bcce25d1b391
MrManoj124 <manojmanorooban@gmail.com>
2026-05-20 18:03:32 +0530

    Add @GetMapping annotation to get data

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
@@ -4,10 +4,7 @@ package Mr.Manoj.E_Comm.Controller;
 import Mr.Manoj.E_Comm.Model.Product;
 import Mr.Manoj.E_Comm.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,4 +26,12 @@ public class ProductController {
         productservice.addProduct(product01);
         return "Successfully added the datas";
     }
+
+    // Retriew the datas
+    @GetMapping("/getProduct")
+    public List<Product>getProduct(){
+        return productservice.getProducts();
+    }
+
+
 }
