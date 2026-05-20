7873fd9e02bef71967dfb3089188028504c5a6d8
MrManoj124 <manojmanorooban@gmail.com>
2026-05-20 18:57:17 +0530

    Add @GetMapping for get simple data

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/ProductController.java
@@ -1,8 +1,8 @@
 package Mr.Manoj.E_Comm.Controller;
 
 
-import Mr.Manoj.E_Comm.Model.Product;
-import Mr.Manoj.E_Comm.Service.ProductService;
+// import Mr.Manoj.E_Comm.Model.Product;
+// import Mr.Manoj.E_Comm.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
 public class ProductController {
-
+    @GetMapping("/")
+    String Greeting() {
+        return "All are Welcome";
+    }
 
 }
