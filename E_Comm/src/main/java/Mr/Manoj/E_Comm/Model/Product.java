a28b63bfa3e8ae523ae4c43ce555058be9d7e3cb
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 09:52:56 +0530

    Add parameterized constructor

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Product.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Product.java
@@ -3,61 +3,36 @@ package Mr.Manoj.E_Comm.Model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-    @Document(collection = "products")
+import java.time.LocalDateTime;
+
+@Document(collection = "products")
     public class Product {
 
         @Id
         private String productId;
 
-        private String productName;
-        private Double price;
-        private Integer stock;
-        private String description;
+        private LocalDateTime orderDate;
+        private Double totalAmount;
+        private String status;
+        private String customerEmail;
 
         //Default Constructor
         public Product() {
         }
 
         //Parameterized constructor
-        public Product(String productName, Double price, Integer stock, String description) {
-            this.productName = productName;
-            this.price = price;
-            this.stock = stock;
-            this.description = description;
+        public Product(LocalDateTime orderDate, Double totalAmount, String status, String customerEmail) {
+            this.orderDate =orderDate;
+            this.totalAmount = totalAmount;
+            this.status = status;
+            this.customerEmail = customerEmail;
         }
 
         // Getters and Setters
 
-        public String getProductName() {
-            return productName;
-        }
 
-        public Double getPrice() {
-            return price;
-        }
 
-        public int getStock() {
-            return stock;
-        }
 
-        public String getDescription() {
-            return description;
-        }
 
-        public void setProductName(String ProductName) {
-            this.productName = productName;
-        }
-
-        public void setName(Double price) {
-            this.price = price;
-        }
-
-        public void setStock(int stock) {
-            this.stock = stock;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    }
+}
 
