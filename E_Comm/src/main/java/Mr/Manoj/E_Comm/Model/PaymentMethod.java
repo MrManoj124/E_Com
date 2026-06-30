b5395ce7506d20be1d4ed792f221a67c35e71144
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 11:32:34 +0530

    feat : create variables and parameterized construtors

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/PaymentMethod.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/PaymentMethod.java
@@ -3,7 +3,22 @@ package Mr.Manoj.E_Comm.Model;
 import org.springframework.data.annotation.Id;
 
 public class PaymentMethod {
+    @Id
+    private String paymentId;
 
+    private String paymentType;
+    private String  accountNumber;
+    private String expiryDate;
+
+    // default constructor
+    public PaymentMethod(){}
+
+    //parameterized constructor
+    public PaymentMethod(String paymentType, String accountNumber, String expiryDate){
+        this.paymentType=paymentType;
+        this.accountNumber=accountNumber;
+        this.expiryDate=expiryDate;
+    }
 
 
 
