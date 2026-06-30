9aab84ac0ed4135d73984b40363eab9cd5139232
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 15:04:50 +0530

    fix : solve the error in the getter and setter

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/PaymentMethod.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/PaymentMethod.java
@@ -1,7 +1,9 @@
 package Mr.Manoj.E_Comm.Model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "payments")
 public class PaymentMethod {
     @Id
     private String paymentId;
