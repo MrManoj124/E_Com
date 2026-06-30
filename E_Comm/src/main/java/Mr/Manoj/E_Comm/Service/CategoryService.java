dbdcc3764ea3ad558470aae752ce7c3091c5fe37
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 15:49:14 +0530

    feat : Create CategoryService.java file and create CRUD operations

---
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Service/CategoryService.java
@@ -0,0 +1,46 @@
+package Mr.Manoj.E_Comm.Service;
+
+import Mr.Manoj.E_Comm.Model.Category;
+import Mr.Manoj.E_Comm.Repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    public CategoryRepository categoryrepo;
+
+    public CategoryService(CategoryRepository categoryRepo){
+        this.categoryrepo=categoryRepo;
+    }
+
+    public Category postcat(Category category){
+        return categoryrepo.save(category);
+    }
+
+    public Category getById(String id) {
+        return categoryrepo.findById(id).orElse(null);
+    }
+
+    public Category update(String id, Category category){
+        Category existing = categoryrepo.findById(id).orElse(null);
+        if(existing != null){
+            existing.setCategoryName(category.getCategoryName());
+            existing.setDescription(category.getDescription());
+            return categoryrepo.save(existing);
+        }
+        return null;
+    }
+
+    public String delete(String id){
+        Category existing = categoryrepo.findById(id).orElse(null);
+        if(existing != null){
+            categoryrepo.deleteById(id);
+            return "Category deleted Succesfully";
+        }
+        return "Category not found";
+    }
+}
