2aa75e6a2adeb15c12c37a1c7e76544785bf2409
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 15:05:37 +0530

    Refactor: solve the error with add getter and setter for the Id field

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Category.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Category.java
@@ -1,7 +1,9 @@
 package Mr.Manoj.E_Comm.Model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "categories")
 public class Category {
 
     @Id
