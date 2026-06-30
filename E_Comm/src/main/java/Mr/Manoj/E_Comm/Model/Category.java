fa5c2844da4a8682b631615a916a99f6f404aaef
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 11:24:18 +0530

    feat : Add getter and setter to related fields in Category.java file

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Category.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Model/Category.java
@@ -1,4 +1,48 @@
 package Mr.Manoj.E_Comm.Model;
 
+import org.springframework.data.annotation.Id;
+
 public class Category {
+
+    @Id
+    private String categoryId;
+
+    private String categoryName;
+    private String description;
+
+    //Deafault parameter
+    public Category(){}
+
+    // Parameterized constructor
+    public Category(String categoryName, String description){
+        this.categoryName=categoryName;
+        this.description=description;
+    }
+
+    // getters and setters
+    public String getCategoryId(){
+        return categoryId;
+    }
+
+    public String getCategoryName(){
+        return categoryName;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setCategoryId(String categoryId){
+        this.categoryId=categoryId;
+    }
+
+    public void setCategoryName(String categoryName){
+        this.categoryName=categoryName;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+
 }
