54a69af841c3082f486de9bfa1669d8e833caae9
MrManoj124 <manojmanorooban@gmail.com>
2026-06-30 18:00:42 +0530

    feat : Add patch operation

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Service/OrderService.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Service/OrderService.java
@@ -1,6 +1,7 @@
 package Mr.Manoj.E_Comm.Service;
 
 import Mr.Manoj.E_Comm.Model.Order;
+import Mr.Manoj.E_Comm.Model.Product;
 import Mr.Manoj.E_Comm.Repository.OrderRepository;
 import Mr.Manoj.E_Comm.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,8 @@ import java.util.List;
 @Service
 public class OrderService {
 
-
+    @Autowired
+    private final ProductRepository productRepo;
 
     @Autowired
     private final OrderRepository orderRepo;
@@ -19,8 +21,22 @@ public class OrderService {
         this.orderRepo=orderRepo;
     }
 
-    public Order postOrd(Order order){
+    public Order postOrder(Order order){
+        double total = calculateTotal(order.getProducts());
+        order.setTotalAmount;
+
         return orderRepo.save(order);
+    }
+
+    public double calculateTotal(List<Product> products){
+        double total = 0;
+        for (Product product : products);
+        total += product.getPrice();
+
+        if(total > 5000){
+            total = total - (total - 0.10);
+        }
+        return total;
     }
 
     public List<Order> getAllOrders(){
@@ -49,5 +65,23 @@ public class OrderService {
             return "Order deleted Successfully";
         }
         return "Order is null";
+    }
+
+    public Order patchOrder(String id, Order order){
+        Order existingOrder = orderRepo.findById(id).orElse(null);
+
+        if(existingOrder != null){
+            return null;
+        }
+
+        if(existingOrder.getCustomerEmail() != null){
+            existingOrder.setCustomerEmail(order.getCustomerEmail());
+        }
+
+        if(existingOrder.getorderDate() != null){
+            existingOrder.setorderDate(order.getorderDate());
+        }
+
+        return orderRepo.save(existingOrder);
     }
 }
