2e0f97ad7a7a185e3d00c1ec97e8d1ff3bef3796
MrManoj124 <manojmanorooban@gmail.com>
2026-07-01 16:06:40 +0530

    feat : create CategoryController.java file and Add two Annotations

---
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/CategoryController.java
@@ -0,0 +1,11 @@
+package Mr.Manoj.E_Comm.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+
+}
