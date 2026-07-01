68a88447c5e91ecac46f9a40336032eeebe0e4dc
MrManoj124 <manojmanorooban@gmail.com>
2026-07-02 00:01:22 +0530

    feat : Successfully implement the CRUD applications

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/CategoryController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/CategoryController.java
@@ -1,11 +1,48 @@
 package Mr.Manoj.E_Comm.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Mr.Manoj.E_Comm.Model.Category;
+import Mr.Manoj.E_Comm.Service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService service;
+
+    @PostMapping
+    public Category postCat(@RequestBody Category category){
+        return service.postCat(category);
+    }
+
+    @GetMapping
+    public List<Category> getAll(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable String id){
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Category update(@PathVariable String id, @RequestBody Category category){
+        return service.update(id, category);
+    }
+
+    @PatchMapping("/{id}")
+    public Category patchcat(@PathVariable String id, @RequestBody Category category){
+        return service.patchcat(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable String id){
+        service.delete(id);
+        return "Category deleted Successfully";
+    }
 
 }
