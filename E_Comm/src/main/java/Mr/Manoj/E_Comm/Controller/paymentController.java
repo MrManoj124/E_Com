267a4b59d3081c278ea5e5a7a5a90a265b43f1da
MrManoj124 <manojmanorooban@gmail.com>
2026-07-02 07:23:47 +0530

    feat : create paymentController.java and state two annotations

---
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/paymentController.java
@@ -0,0 +1,9 @@
+package Mr.Manoj.E_Comm.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+public class paymentController {
+}
