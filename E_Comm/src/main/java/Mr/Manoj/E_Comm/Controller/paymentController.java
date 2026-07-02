bddee699d4198da20654ae6e0c7b91ef6c8df403
MrManoj124 <manojmanorooban@gmail.com>
2026-07-02 08:51:06 +0530

    fix : add delete operation

--- E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/paymentController.java
+++ E_Comm/src/main/java/Mr/Manoj/E_Comm/Controller/paymentController.java
@@ -1,9 +1,51 @@
 package Mr.Manoj.E_Comm.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Mr.Manoj.E_Comm.Model.PaymentMethod;
+import Mr.Manoj.E_Comm.Service.PaymentMethodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/payments")
 public class paymentController {
+
+    @Autowired
+    private PaymentMethodService service;
+
+    @PostMapping
+    public PaymentMethod addPayment(@RequestBody PaymentMethod payment) {
+        return service.addPayment(payment);
+    }
+
+    @GetMapping
+    public List<PaymentMethod> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public PaymentMethod getPaymentById(@PathVariable String id) {
+        return service.getPaymentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public PaymentMethod updatePayment(@PathVariable String id, @RequestBody PaymentMethod payment) {
+        return service.updatePayement(id, payment);
+    }
+
+    @PatchMapping("/{id}")
+    public PaymentMethod patchPayment(@PathVariable String id, @RequestBody PaymentMethod payment) {
+        return service.patchPayment(id, payment);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletePayment(@PathVariable String id) {
+        return service.deletePayment(id);
+    }
+
+
+
+
+
 }
